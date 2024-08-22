05bbc0b7174afc85baf6b47e21592a786952f8bd
twinksaji223@gmail.com <muhammedshebinva@gmail.com>
2024-08-22 18:26:16 +0530

    css updated

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -18,9 +18,9 @@ const HeroSection = () => {
     return (
         <div className='text-center'>
             <div className='flex flex-col gap-5 my-10'>
-                <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 Job Hunt Website</span>
-                <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Jobs</span></h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid aspernatur temporibus nihil tempora dolor!</p>
+                <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>SCMS School of Engineering & Technology</span>
+                <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#E24C0C;]'>Placement Drives</span></h1>
+                <p>The SCMS School of Engineering and Technology (SSET) is envisioned as a premier institution providing exceptional technology-related education</p>
                 <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
                     <input
                         type="text"
@@ -29,7 +29,7 @@ const HeroSection = () => {
                         className='outline-none border-none w-full'
 
                     />
-                    <Button onClick={searchJobHandler} className="rounded-r-full bg-[#6A38C2]">
+                    <Button onClick={searchJobHandler} className="rounded-r-full bg-[#E24C0C]">
                         <Search className='h-5 w-5' />
                     </Button>
                 </div>
